3407a111d041a9693a77ea96974098d0c53754c8
Karina Dalca <karinadalca@gmail.com>
2017-06-28 11:29:09 -0700

    moved index.html outside dist (#8)

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var axios = require('axios');
-//var database = require('../database/index.js');
+var database = require('../database/index.js');
 var Sequelize = require('sequelize');
 
 var app = express();
 app.use(bodyParser.json());
 app.use( bodyParser.urlencoded({ extended: true }) );
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/'));
 
 
 
