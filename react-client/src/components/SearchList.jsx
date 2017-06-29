b7ea0fc02c016eed1dabbb4e167711dcde9102f5
Tyler Pate <tylerpate@Tylers-MacBook-Pro.local>
2017-06-29 16:44:13 -0700

    Code clean up, and implement addBookToDatabase

---
+++ react-client/src/components/SearchList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import SearchListItem from './SearchListItem.jsx';
+
+const SearchList = (props) => {
+  console.log('Inside Search List', props.searchedBooks)
+return (
+    <div id="searchResults">
+      <table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+        <thead>
+          <tr>
+            <th className="mdl-data-table__cell--non-numeric">Title</th>
+            <th>Author</th>
+            <th>Year Published</th>
+            <th>Genre</th>
+            <th>Language</th>
+            <th>Cover</th>
+            <th>Audio</th>
+            <th>Add</th>
+          </tr>
+        </thead>
+        <tbody>
+          { props.searchedBooks.map((book, index) => <SearchListItem key={index} book={book} addBookToLibrary={props.addBookToLibrary} />)}
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
+
+
+
+export default SearchList;
