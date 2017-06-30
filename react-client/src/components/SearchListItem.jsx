b9157736c3e32fd125c64b57bdc451dfcace7ea2
Brendan Burns <brendanmioburns@gmail.com>
2017-06-30 16:44:26 -0700

    Feat/twonewmodals (#23)
    * add boron
    
    * update index.jsx to reflect modals
    
    * edit attribute, clean up spacing
    
    * rename Modal file, add code for ModalSearchList
    
    * add code for ModalBookOnShelf
    
    * add styles
    
    * add id
    
    * add SearchList

--- react-client/src/components/SearchListItem.jsx
+++ react-client/src/components/SearchListItem.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
 const SearchListItem = (props) => {
-    return (
-        <tr>
-        <td className="mdl-data-table__cell--non-numeric">{props.book.title}</td>
-            <td>{props.book.author}</td>
-            <td>{props.book.yearPublished}</td>
-            <td>{props.book.genre}</td>
-            <td>{props.book.lang}</td>
-            <td><img className="thumbnail" src={props.book.cover} style={{zoom: '25%'}}/></td>
-            <td><i className="material-icons">headset</i></td>
-            <td onClick={ () => { props.addBookToLibrary(props.book) } }><a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a></td>
-        </tr>
-
-
-    )
+  return (
+    <tr>
+      <td className="mdl-data-table__cell--non-numeric">{props.book.title}</td>
+      <td>{props.book.author}</td>
+      <td>{props.book.yearPublished}</td>
+      <td>{props.book.genre}</td>
+      <td>{props.book.lang}</td>
+      <td><img className="thumbnail" src={props.book.cover} /></td>
+      <td><i className="material-icons">headset</i></td>
+      <td onClick={ () => { props.addBookToLibrary(props.book) } }><a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons addBook">add</i></a></td>
+    </tr>
+  )
 };
-
-
 
 export default SearchListItem;
