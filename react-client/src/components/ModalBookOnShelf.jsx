58f9016aac360aa4395eb8fab4a0995c58df9cf7
Brendan Burns <brendanmioburns@gmail.com>
2017-07-03 14:53:59 -0700

    center audio buttons, fix cursor hovers

--- react-client/src/components/ModalBookOnShelf.jsx
+++ react-client/src/components/ModalBookOnShelf.jsx
@@ -9,7 +9,7 @@ class ModalBookOnShelf extends React.Component {
 
     this.styles = {
       btn1: {
-        margin: '1em auto',
+        margin: 'auto',
         padding: '1em 2em',
         outline: 'none',
         fontSize: 16,
@@ -20,7 +20,7 @@ class ModalBookOnShelf extends React.Component {
         float: 'right'
       },
       btn2: {
-        margin: '1em auto',
+        margin: 'auto',
         padding: '1em 2em',
         outline: 'none',
         fontSize: 16,
@@ -29,6 +29,17 @@ class ModalBookOnShelf extends React.Component {
         color: '#FFFFFF',
         border: 'none',
         float: 'left'
+      },
+      btn3: {
+        margin: 'auto',
+        float: 'none',
+        outline: 'none',
+        color: 'black',
+        border: 'none',
+        zoom: 2,
+        display: 'flex',
+        verticalAlign: 'middle',
+        textDecoration: 'none'
       },
       container: {
         padding: '2em',
@@ -70,12 +81,15 @@ class ModalBookOnShelf extends React.Component {
           <h4 className="bookAuthor">by {this.props.book.author}</h4>
           <h5 className="bookDate">{this.props.book.yearPublished}</h5>
           <p className="bookSummary">{this.removeHTMLtags(this.props.book.description)}</p>
-          <h5 className="bookAudio">Click to download audio version:  <a href={this.props.book.audio} target="_blank" style={{color:'black'}}><i className="material-icons headset">headset</i></a></h5>
+
           <button className="modalClose" onClick={ () =>{
             this.hideModal.bind(this)();
             this.props.deleteBookFromLibrary(this.props.book);
           }
           } style={this.styles.btn1}>Remove</button>
+          <button id="audioButton">
+          <a href={this.props.book.audio} target="_blank" style={this.styles.btn3}><i className="material-icons headset">headset</i></a>
+          </button>
           <a className="modalClose" style={this.styles.btn1} href={this.props.book.content} target="_blank" style={this.styles.btn2}>Read</a>
         </div>
       </Modal>
